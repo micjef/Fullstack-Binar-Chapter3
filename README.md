1633cfcd7b2b927e888cad4d8b0796916d519bb2
micjef <michaeljeffrysetiawan@gmail.com>
2022-03-25 11:30:07 +0700

    FSW Chapter 3

---
+++ README.md
@@ -0,0 +1,2 @@
+# binar-car-rental
+challenge-3 binar fullstack web
---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(".owl-carousel").owlCarousel({
+  center: true,
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 2,
+    },
+  },
+});
+
+var owl = $(".owl-carousel");
+owl.owlCarousel();
+$(".btn-right").click(function () {
+  owl.trigger("next.owl.carousel");
+});
+$(".btn-left").click(function () {
+  owl.trigger("prev.owl.carousel", [300]);
+});
